d351fab96a26c82e9dccf5a7bb4b9ac5f4eee1f7
Dima <dima@MacBook-Pro-User.local>
2017-11-09 00:05:09 +0200

    Now with eslint

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+'use strict';
+
 const TelegramBot = require('node-telegram-bot-api');
-const config = require('./config.json')
-const fs = require('fs');
-const apw = require('./appendHomeWork')
+const config = require('./config.json');
+const apw = require('./appendHomeWork');
 
 
-const token = config.token
-const bot = new TelegramBot(token, {polling: true});
+const token = config.token;
+const bot = new TelegramBot(token, { polling: true });
 
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -20,13 +21,13 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   bot.sendMessage(chatId, resp);
 });
 
-// let taskArr = [];
-// const respArr = ['Enter deadline: ', 'Enter hometask: ','Enter Subject: ']
+let taskArr = [];
 let taskFlag = false;
 bot.onText(/\/new/, (msg) => {
   const chatId = msg.chat.id;
   taskFlag = true;
-  bot.sendMessage(chatId, 'Enter your asingment (Subject; Hometask; Deadline): ')
+  bot.sendMessage(chatId,
+    'Enter your asingment (Subject; Hometask; Deadline): ');
 });
 
 bot.on('message', (msg) => {
@@ -34,8 +35,8 @@ bot.on('message', (msg) => {
   if (taskFlag) {
     taskArr = msg.text.split('; ');
     console.log(taskArr);
-    apw.appendHomeWork(taskArr[0],taskArr[1],taskArr[2])
-    sendMessage(chatId, 'Hometask has been sent to students successfully')
+    apw.appendHomeWork(taskArr[0], taskArr[1], taskArr[2]);
+    bot.sendMessage(chatId, 'Hometask has been sent to students successfully');
     taskFlag = false;
   }
-})
+});
