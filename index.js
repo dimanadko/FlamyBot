04f0137bfadd4dedce2528faa565eefb8f0f8cf0
Dima <dima@MacBook-Pro-User.local>
2017-11-09 21:33:06 +0200

    Get hometask with '/get (Subject)' now with eslint

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config.json');
 const apw = require('./appendHomeWork');
-const fs = require('fs')
+const fs = require('fs');
 
 
 const token = config.token;
 const bot = new TelegramBot(token, { polling: true });
 
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/\/echo (.+)/, (msg) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
@@ -37,16 +37,16 @@ bot.onText(/\/new/, (msg) => {
 
 bot.onText(/\/get (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
-  let taskData = JSON.parse(fs.readFileSync('./dataBase/data.json'));
-  if (Object.keys(taskData).includes(match[1])){
-    let subjTaskArr = taskData[match[1]];
+  const taskData = JSON.parse(fs.readFileSync('./dataBase/data.json'));
+  if (Object.keys(taskData).includes(match[1])) {
+    const subjTaskArr = taskData[match[1]];
     console.log(subjTaskArr);
     if (subjTaskArr.length !== 0) {
       subjTaskArr.forEach((item) => {
         bot.sendMessage(chatId, item.task + ' - ' + item.deadline);
         if (item.fileId) bot.sendDocument(chatId, item.fileId);
       });
-    } else {bot.sendMessage(chatId, 'No hometask yet');}
+    } else { bot.sendMessage(chatId, 'No hometask yet'); }
   } else bot.sendMessage(chatId, 'No such subject exist');
 });
 
@@ -54,9 +54,9 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   if (taskFlag4) {
-    if (msg.text !== 'No'){
+    if (msg.text !== 'No') {
       if (msg.document) {
-        taskArr.fileId = msg.document.file_id
+        taskArr.fileId = msg.document.file_id;
       } else {
         bot.sendMessage(chatId, 'Not a file!');
       }
@@ -75,7 +75,8 @@ bot.on('message', (msg) => {
     // console.log(taskArr);
     bot.sendMessage(chatId, 'Would you like to add file or image?',
       {
-        "reply_markup": {"keyboard": [[{ text: "No" }]], "one_time_keyboard": true}
+        'reply_markup': { 'keyboard': [[{ text: 'No' }]],
+          'one_time_keyboard': true }
       });
     taskFlag3 = false;
     taskFlag4 = true;
@@ -85,7 +86,8 @@ bot.on('message', (msg) => {
     // console.log(taskArr);
     bot.sendMessage(chatId, 'Enter hometask deadline: ',
       {
-        "reply_markup": {"keyboard": [[{ text: "auto" }]], "one_time_keyboard": true},
+        'reply_markup': { 'keyboard': [[{ text: 'auto' }]],
+          'one_time_keyboard': true },
       });
     taskFlag2 = false;
     taskFlag3 = true;
